fa8ae676de951a5279cb534205fcf53d8bf3db1b
surcula <david.gaillet@live.fr>
2024-02-15 17:53:17 +0100

    Ajoutez des fichiers projet.

---
+++ MasterMind/MasterMindGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind.MasterMind
+{
+    /*
+     * Un objet
+     * Une réponse de 5 couleurs
+     * 10 Lignes
+     * 5 couleurs (Bleu, Jaune, Vert, Rouge, Noir)
+     *
+     *-> On demande la réponse
+     *->boucle
+     *-->On demande une solution
+     *-->On vérifie Si oui -> victoire Si non on recommence en indiquant l'aide.
+     * 
+     */
+
+    public class MasterMindGame
+    {
+        //Création du tableau colors
+        private string[] colorsChoices = {"Bleu","Jaune","Vert","Rouge","Noir"};
+        // La réponse
+        public string[] response = new string[5];
+        // Le jeu
+        public string[,] gameBoard = new string[10, 5];
+        // La sélection
+        int selectedOptionGame = 0;
+
+
+        //Méthode pour créer la réponse
+        public void CreateReponse()
+        {
+            Console.Clear();
+            Console.WriteLine("Choix de la réponse:");
+
+            int centerX = Console.WindowWidth / 2;
+            int centerY = Console.WindowHeight / 2 - colorsChoices.Length / 2 - 2;
+            int currentIndex = 0;
+            int selectedColorsCount = 0;
+
+            while (selectedColorsCount < 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Choix de la réponse:");
+                Console.WriteLine($"Étape {selectedColorsCount + 1} sur 5\n");
+
+                for (int i = 0; i < colorsChoices.Length; i++)
+                {
+                    if (i == currentIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    Console.SetCursorPosition(centerX - colorsChoices[i].Length / 2, centerY + i);
+                    Console.WriteLine(colorsChoices[i]);
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        currentIndex = (currentIndex == 0) ? colorsChoices.Length - 1 : currentIndex - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        currentIndex = (currentIndex == colorsChoices.Length - 1) ? 0 : currentIndex + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        response[selectedColorsCount] = colorsChoices[currentIndex];
+                        selectedColorsCount++;
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using MasterMind;
+using MasterMind.MasterMind;
+
+class Program
+{ 
+    static int selectedOption = 0;
+    static string[] menuOptions = { "New Game", "Exit" };
+
+
+    static void Main(string[] args)
+    {
+       
+
+        //Boucle du menu
+        while (true)
+        {
+            Console.Clear();
+            DrawMenu();
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                selectedOption = (selectedOption - 1 + menuOptions.Length) % menuOptions.Length;
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                selectedOption = (selectedOption + 1) % menuOptions.Length;
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                if (selectedOption == 0)
+                {
+                    StartGame();
+                }
+                else if (selectedOption == 1)
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+
+        //Affichage du menu
+        static void DrawMenu()
+        {
+            int centerX = Console.WindowWidth / 2;
+            int centerY = Console.WindowHeight / 2;
+
+            Console.SetCursorPosition(centerX, centerY - (menuOptions.Length / 2));
+            Console.WriteLine("MASTERMIND");
+            for (int i = 0; i < menuOptions.Length; i++)
+            {
+                if (i == selectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.SetCursorPosition(centerX - (menuOptions[i].Length / 2), Console.CursorTop + 1);
+                Console.WriteLine(menuOptions[i]);
+            }
+        }
+
+        //Le jeu
+        static void StartGame()
+        {
+            MasterMindGame mastermind = new MasterMindGame();
+            mastermind.CreateReponse();
+            
+        }
+
+    }
+}
